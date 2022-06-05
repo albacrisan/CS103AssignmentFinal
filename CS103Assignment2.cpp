597629e38346bcf7552f3244ad41db6fced10f90
MattNZ305 <mathewclarkecox@gmail.com>
2022-06-05 20:32:55 +1200

    Login Stuff Done

--- CS103Assignment2.cpp
+++ CS103Assignment2.cpp
@@ -11,56 +11,184 @@
 #include <stdlib.h>
 using namespace std;
 
-struct universityClasses {
-	string courseCode; //Three letters for course & 3 numbers: first 2 representing the year, 3rd the semester 1, 2 or 3 for summer school
-	string courseName;
-	int	teacherassigned;
-	float cost;
+struct Parent {
+	string fName;
+	string LName;
+	long int contactNumber;
 };
+struct Teacher {
+	string fName;
+	string lName;
 
-struct teacher {
-	int title;  //Mr  = 1, Mrs = 2, Miss = 3, Dr = 4; not super necessary, can remove
-	string teacherFirstName;
-	string teacherlastName;
-	int teacherID;
-	string teacherPassword;
+};
+struct classStr {
+	string code;
+	float studentGrade;
+	float courseCost;
+	Teacher teacher;
 };
 
 struct Student {
-	string studentFirstName;
-	string studentlastName;
-	int studentID;
-	int studentPhoneNumber;
-	int studentEmail;
-	//string studentPassword, removed as we discussed student no access - if correct pls delete
-	string parentFirstName; // Start of emergency contact details
-	string parentlastName;
-	long int parentPhoneNumber;
-	string CoursesAssigned;
-	bool courseCompleted; // “Y / N”
-	bool paid; // “Y / N”;
-	float SUM; // -Total tuition per semester / year
+	long int ID;
+	string PW;
+	string fName;
+	string lName;
+	Parent parent;
+	classStr class1;
+	classStr class2;
+	classStr class3;
+	classStr class4;
+
+	Student() {
+		ID = 0;
+		PW = "PW";
+		fName = "FirstName";
+		lName = "LastName";
+
+	}
 };
 
+vector <Student> Registration(vector<Student>& student);
+vector <Student> StudentLogin(vector<Student>& student);
+void adminLogin();
+void adminDatabase();
 
 
-struct Grades {
-	string courseCode;
-	float averageAllStudentGradeperclass;
-	char letterGrade;
-	float averageOverallmark;  // will be result of a function
-};
+int main() {
+	vector <Student> student;
+	vector <Student> studentFromFile;
+	int menuChoice, loginChoice;
+	cout << "***University Database***\n\n\n";
+	cout << "Press 1 to Login\nPress 2 to Register\nPress 3 to exit Program\nEnter your choice: "; cin >> menuChoice;
+	switch (menuChoice) {
+	case 1:
+		cout << "\n\nPress 1 for student login\nPress 2 for Admin login\nEnter your choice: "; cin >> loginChoice;
+		if (loginChoice == 1) {
+			cout << "\n\nDirecting to Student Login...";
+			Sleep(500);
+			StudentLogin(student);
+			break;
+		}
+		else if (loginChoice == 2) {
+			cout << "\n\nDirecting to Admin Login...";
+			Sleep(500);
+			adminLogin();
+			break;
+		}
+		else {
+			cout << "Invalid input. Press 1 or 2";
+			Sleep(1000);
+			system("cls");
+			main();
+			break;
+		}
+	case 2:
+		cout << "\n\nDirecting to Student Registration...";
+		Sleep(500);
+		Registration(student);
+		break;
+	case 3:
+		cout << "\n\nClosing App...";
+		Sleep(500);
+		exit(1);
+	default:
+		cout << "\n\ninvalid input. Press 1,2 or 3";
+		Sleep(1000);
+		system("cls");
+		main();
+	}
 
 
-int main()
-{
-	//string admin_pass = "admin";
-	//cout << "Enter the admin password: ";
-	//string password;
-	//cin >> password;
-
-	cout << "Hello World!\n";
-	cout << "Update by Matt\n";
 }
 
-// this is attempt with new branch
+vector <Student> Registration(vector<Student>& student) {
+	system("cls");
+	Student s;
+	cout << "***Student Registration***\n\n";
+	cout << "Please Enter your Student ID (6 digit Number): "; cin >> s.ID;
+	cout << "Create your password. Password must container uppercase,lowercase & number: "; cin >> s.PW;
+	cout << "Enter your first name: "; cin >> s.fName;
+	cout << "Enter your last name "; cin >> s.lName;
+
+	student.push_back(s);
+	fstream StudentDatabase("stDB.csv", ios::app);
+	StudentDatabase << s.ID << "," << s.PW << endl;
+	StudentDatabase.close();
+	cout << "\n\nRegistration Successfull\nDirecting to main menu...";
+	Sleep(1000);
+	system("cls");
+	main();
+	return(student);
+
+}
+vector <Student> StudentLogin(vector<Student>& student) {
+	system("cls");
+	Student s;
+	string attemptPW;
+	long int attemptID;
+	int counter = 3, flag = 0;
+	while (counter != 0) {
+		cout << "***Student Login***\n\n";
+		cout << "Enter ID: "; cin >> attemptID;
+		cout << "Enter PW: "; cin >> attemptPW;
+
+		fstream StudentDatabase("stDB.csv", ios::in);
+		while (StudentDatabase >> s.ID >> s.PW) {
+			if (attemptID == s.ID && attemptPW == s.PW) {// not inputting the right variable for it to read off the spreadsheet
+				flag = 1;
+			}
+		}
+		switch (flag) {
+		case 0:
+			counter--;
+			cout << "\nIncorrect ID or Password\n" << counter << " login attempts remaining\n";
+			break;
+
+		case 1:
+			cout << "\nLogin successfull\nDirecting you to database...\n";
+			Sleep(1000);
+			break;
+		}
+	}
+	cout << "Closing app...\n\n\n\n";
+	Sleep(1000);
+	exit(1);
+	return(student);
+}
+
+void adminLogin() {
+	system("cls");
+	int counter = 3;
+	long int adminID = 000000, enteredID;
+	string adminPW = "Password", enteredPW;
+	while (counter != 0) {
+		cout << "***Admin Login***\n";
+		cout << "Enter Admin ID: "; cin >> enteredID;
+		cout << "Enter Admin PW: "; cin >> enteredPW;
+		if (adminID == enteredID) {
+			cout << "\n\nLogin Successful\nWelcome admin. Opening Database now...";
+			Sleep(1000);
+			adminDatabase();
+			break;
+
+
+		}
+		else {
+			counter--;
+			cout << "\nInvalid ID or PW " << counter << " attempts remaining\n";
+		}
+	}
+	cout << "0 login attempts remaining\n Closing app...";
+	exit(3);
+}
+
+void adminDatabase() {
+	system("cls");
+	cout << "***Admin Database Access***\n\n";
+}
+void studentDatabase() {
+	system("cls");
+	cout << "***Student Database Access***\n\n";
+}
+
+
